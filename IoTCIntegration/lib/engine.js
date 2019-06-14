5b04182285bd7d5c41557099113ea8bcf4e37011
Benjamin Cabé <kartben@users.noreply.github.com>
2019-06-14 19:23:25 +0200

    add support for properties
    Signed-off-by: Benjamin Cabé <benjamin.cabe@microsoft.com>

--- IoTCIntegration/lib/engine.js
+++ IoTCIntegration/lib/engine.js
@@ -4,19 +4,16 @@
  */
 
 const crypto = require('crypto');
-const request = require('request-promise-native');
+
 const Device = require('azure-iot-device');
-const DeviceTransport = require('azure-iot-device-http');
-const util = require('util');
+const DeviceTransport = require('azure-iot-device-mqtt');
+var ProvisioningTransport = require('azure-iot-provisioning-device-mqtt').Mqtt;
+var SymmetricKeySecurityClient = require('azure-iot-security-symmetric-key').SymmetricKeySecurityClient;
+var ProvisioningDeviceClient = require('azure-iot-provisioning-device').ProvisioningDeviceClient;
 
 const StatusError = require('../error').StatusError;
 
 const registrationHost = 'global.azure-devices-provisioning.net';
-const registrationSasTtl = 3600; // 1 hour
-const registrationApiVersion = `2018-11-01`;
-const registrationStatusQueryAttempts = 10;
-const registrationStatusQueryTimeout = 2000;
-const minDeviceRegistrationTimeout = 60*1000; // 1 minute
 
 const deviceCache = {};
 
@@ -25,8 +22,10 @@ const deviceCache = {};
  * @param {{ idScope: string, primaryKeyUrl: string, log: Function, getSecret: (context: Object, secretUrl: string) => string }} context 
  * @param {{ deviceId: string }} device 
  * @param {{ [field: string]: number }} measurements 
+ * @param {{ [field: string]: Object }} properties 
+ * @param { String } timestamp 
  */
-module.exports = async function (context, device, measurements, timestamp) {
+module.exports = async function (context, device, measurements, properties, timestamp) {
     if (device) {
         if (!device.deviceId || !/^[a-z0-9\-]+$/.test(device.deviceId)) {
             throw new StatusError('Invalid format: deviceId must be alphanumeric, lowercase, and may contain hyphens.', 400);
@@ -39,23 +38,34 @@ module.exports = async function (context, device, measurements, timestamp) {
         throw new StatusError('Invalid format: invalid measurement list.', 400);
     }
 
+    // TODO validate properties
+
     if (timestamp && isNaN(Date.parse(timestamp))) {
         throw new StatusError('Invalid format: if present, timestamp must be in ISO format (e.g., YYYY-MM-DDTHH:mm:ss.sssZ)', 400);
     }
 
-    const client = Device.Client.fromConnectionString(await getDeviceConnectionString(context, device), DeviceTransport.Http);
-
     try {
+        const client = Device.Client.fromConnectionString(await getDeviceConnectionString(context, device), DeviceTransport.Mqtt);
+        context.log('DEVICE CACHE: ', deviceCache);
+    
         const message = new Device.Message(JSON.stringify(measurements));
 
         if (timestamp) {
             message.properties.add('iothub-creation-time-utc', timestamp);
         }
 
-        await util.promisify(client.open.bind(client))();
-        context.log('[HTTP] Sending telemetry for device', device.deviceId);
-        await util.promisify(client.sendEvent.bind(client))(message);
-        await util.promisify(client.close.bind(client))();
+        await client.open();
+        context.log('[MQTT] Sending telemetry for device ', device.deviceId);
+        await client.sendEvent(message);
+        if (properties) {
+            context.log('[MQTT] Get twin', device.deviceId);
+            var twin = await client.getTwin();
+            context.log('[MQTT] Obtained twin for device ', device.deviceId);
+            await twin.properties.reported.update(properties);
+            context.log('[MQTT] Updated twin for device ', device.deviceId);
+        }
+        await client.close();
+        context.log('[MQTT] Client closed for ', device.deviceId);
     } catch (e) {
         // If the device was deleted, we remove its cached connection string
         if (e.name === 'DeviceNotFoundError' && deviceCache[device.deviceId]) {
@@ -90,87 +100,15 @@ async function getDeviceConnectionString(context, device) {
         return deviceCache[deviceId].connectionString;
     }
 
-    const connStr = `HostName=${await getDeviceHub(context, device)};DeviceId=${deviceId};SharedAccessKey=${await getDeviceKey(context, deviceId)}`;
+    var symmetricKey = await getDeviceKey(context, deviceId);
+    var provisioningSecurityClient = new SymmetricKeySecurityClient(deviceId, symmetricKey);
+    var provisioningClient = ProvisioningDeviceClient.create(registrationHost, context.idScope, new ProvisioningTransport(), provisioningSecurityClient);
+
+    var registrationResult = await provisioningClient.register();
+
+    const connStr = 'HostName=' + registrationResult.assignedHub + ';DeviceId=' + registrationResult.deviceId + ';SharedAccessKey=' + symmetricKey;
     deviceCache[deviceId].connectionString = connStr;
     return connStr;
-}
-
-/**
- * Registers this device with DPS, returning the IoT Hub assigned to it.
- */
-async function getDeviceHub(context, device) {
-    const deviceId = device.deviceId;
-    const now = Date.now();
-
-    // A 1 minute backoff is enforced for registration attempts, to prevent unauthorized devices
-    // from trying to re-register too often.
-    if (deviceCache[deviceId] && deviceCache[deviceId].lasRegisterAttempt && (now - deviceCache[deviceId].lasRegisterAttempt) < minDeviceRegistrationTimeout) {
-        const backoff = Math.floor((minDeviceRegistrationTimeout - (now - deviceCache[deviceId].lasRegisterAttempt)) / 1000);
-        throw new StatusError(`Unable to register device ${deviceId}. Minimum registration timeout not yet exceeded. Please try again in ${backoff} seconds`, 403);
-    }
-
-    deviceCache[deviceId] = {
-        ...deviceCache[deviceId],
-        lasRegisterAttempt: Date.now()
-    }
-
-    const sasToken = await getRegistrationSasToken(context, deviceId);
-
-    const registrationOptions = {
-        url: `https://${registrationHost}/${context.idScope}/registrations/${deviceId}/register?api-version=${registrationApiVersion}`,
-        method: 'PUT',
-        json: true,
-        headers: { Authorization: sasToken },
-        body: { registrationId: deviceId }
-    };
-
-    try {
-        context.log('[HTTP] Initiating device registration');
-        const response = await request(registrationOptions);
-
-        if (response.status !== 'assigning' || !response.operationId) {
-            throw new Error('Unknown server response');
-        }
-
-        const statusOptions = {
-            url: `https://${registrationHost}/${context.idScope}/registrations/${deviceId}/operations/${response.operationId}?api-version=${registrationApiVersion}`,
-            method: 'GET',
-            json: true,
-            headers: { Authorization: sasToken }
-        };
-
-        // The first registration call starts the process, we then query the registration status
-        // every 2 seconds, up to 10 times.
-        for (let i = 0; i < registrationStatusQueryAttempts; ++i) {
-            await new Promise(resolve => setTimeout(resolve, registrationStatusQueryTimeout));
-
-            context.log('[HTTP] Querying device registration status');
-            const statusResponse = await request(statusOptions);
-
-            if (statusResponse.status === 'assigning') {
-                continue;
-            } else if (statusResponse.status === 'assigned' && statusResponse.registrationState && statusResponse.registrationState.assignedHub) {
-                return statusResponse.registrationState.assignedHub;
-            } else if (statusResponse.status === 'failed' && statusResponse.registrationState && statusResponse.registrationState.errorCode === 400209) {
-                throw new StatusError('The device may be unassociated or blocked', 403);
-            } else {
-                throw new Error('Unknown server response');
-            }
-        }
-
-        throw new Error('Registration was not successful after maximum number of attempts');
-    } catch (e) {
-        throw new StatusError(`Unable to register device ${deviceId}: ${e.message}`, e.statusCode);
-    }
-}
-
-async function getRegistrationSasToken(context, deviceId) {
-    const uri = encodeURIComponent(`${context.idScope}/registrations/${deviceId}`);
-    const ttl = Math.round(Date.now() / 1000) + registrationSasTtl;
-    const signature = crypto.createHmac('sha256', new Buffer(await getDeviceKey(context, deviceId), 'base64'))
-        .update(`${uri}\n${ttl}`)
-        .digest('base64');
-    return`SharedAccessSignature sr=${uri}&sig=${encodeURIComponent(signature)}&skn=registration&se=${ttl}`;
 }
 
 /**
@@ -186,6 +124,10 @@ async function getDeviceKey(context, deviceId) {
         .digest()
         .toString('base64');
 
-    deviceCache[deviceId].deviceKey = key;
+    deviceCache[deviceId] = {
+        ...deviceCache[deviceId],
+        deviceId: key
+    } 
+
     return key;
 }
